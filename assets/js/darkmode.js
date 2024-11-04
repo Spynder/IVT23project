b15bed1fe607875a0cb7a369614e759ffc4ebc0d
Spynder <19329095+Spynder@users.noreply.github.com>
2024-11-04 16:17:30 +0300

    dark mode + calculus questions 2 module

---
+++ assets/js/darkmode.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	let darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+		
+	if(localStorage.getItem("darkMode") !== null) {
+		console.log('found', localStorage.getItem("darkMode"))
+		darkMode = localStorage.getItem("darkMode") == "dark";
+	}
+
+	if (darkMode) {
+		$("html").toggleClass("dark-mode");
+		$("#darkmode").attr("checked", true);
+	}
+
+	$("#darkmode").click(function() {
+		$("html").get(0).style.setProperty("--transition-time", "0.25s");
+		$("html").toggleClass("dark-mode");
+		let isDark = $("html").hasClass("dark-mode");
+		localStorage.setItem("darkMode", isDark ? "dark" : "light");
+	});
+});
